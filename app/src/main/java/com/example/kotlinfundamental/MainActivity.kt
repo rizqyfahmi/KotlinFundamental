1c7c56bd5b9e73d3a8b7aae5098075540475baf1
rizqyfahmi <rizqyfahmi@yahoo.com>
2021-07-11 23:30:43 +0700

    Hello RxJava and RxAndroid

--- app/src/main/java/com/example/kotlinfundamental/MainActivity.kt
+++ app/src/main/java/com/example/kotlinfundamental/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.kotlinfundamental
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.kotlinfundamental.databinding.ActivityMainBinding
+import io.reactivex.rxjava3.core.Observable
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            btnObserve.setOnClickListener {
+                // "just" is an operator that converts its parameters into an observable and emit it
+                Observable.just("Hello RxJava and RxAndroid").subscribe {
+                    Log.d("Reactive Programming", "Result: ${it}")
+                }
+            }
+        }
+
+        setContentView(binding.root)
     }
 }
