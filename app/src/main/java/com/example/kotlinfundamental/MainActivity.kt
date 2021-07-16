9524f185666a996acb484e9d7a86f2986d4c64b9
rizqyfahmi <rizqyfahmi@yahoo.com>
2021-07-17 01:02:18 +0700

    Single, Maybe, and Completable

--- app/src/main/java/com/example/kotlinfundamental/MainActivity.kt
+++ app/src/main/java/com/example/kotlinfundamental/MainActivity.kt
@@ -4,8 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.example.kotlinfundamental.databinding.ActivityMainBinding
-import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.core.*
 import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.observables.ConnectableObservable
 import java.util.concurrent.TimeUnit
@@ -20,55 +19,85 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater).apply {
             btnObserve.setOnClickListener {
-                /** --------------------------------- Start | Cold Observable --------------------------------- **/
+                /** --------------------------------- Start | Single --------------------------------- **/
                 /**
-                 * "Observable" starts its emission right after observer subscribe on it
-                 * Cold Observable creates a data producer for each subscriber. In the sense Cold Observable only emits an item/event when it has a observer
-                 * That's why the value received by each observer is different because Cold Observable will always generate a random value for every observer
-                 * */
-                val cold : Observable<Long> = Observable.create<Long> {
-                    it.onNext(Random.nextLong(100))
-                    it.onComplete()
+                 * Single is an Observable which only emits one item or throws an error.
+                 * Single isn't like an Observable we wrote before. The only noticeable difference is the observer definition where "onNext()" and "onComplete()" of Observable has been combined to "onSuccess()", as the stream has only one single item to emit.
+                 * Single is like promise in Javascript. A promise is an object that may produce a item or throw an error.
+                 * Typically example is a network call, using retrofit you return an Observable or Flowable. You usually care for the response once you can replace this with Single<T>
+                 **/
+                val single = Single.just(1)
+                val singleObserver = object: SingleObserver<Int> {
+                    override fun onSubscribe(d: Disposable?) {
+                        Log.d("Reactive Programming", "Single onSubscribe!")
+                    }
+
+                    override fun onSuccess(t: Int?) {
+                        Log.d("Reactive Programming", "Single onSuccess!: ${t.toString()}")
+                    }
+
+                    override fun onError(e: Throwable?) {
+                        Log.d("Reactive Programming", "Single onError!: ${e?.message}")
+                    }
                 }
 
-                cold.subscribe {
-                    Log.d("Reactive Programming", "Cold Observer#1 - onNext!: ${it}")
-                }
+                single.subscribe(singleObserver)
+                /** --------------------------------- End | Single --------------------------------- **/
 
-                cold.subscribe {
-                    Log.d("Reactive Programming", "Cold Observer#2 - onNext!: ${it}")
-                }
-                /** --------------------------------- End | Cold Observable --------------------------------- **/
-
-
-                /** --------------------------------- Start | Hot Observable --------------------------------- **/
+                /** --------------------------------- Start | Maybe --------------------------------- **/
                 /**
-                 * ConnectableObservable start its emission the moment wa call "connect()"
-                 * Hot Observable creates a data producer first, then each subscriber gets the data from one producer. It produces data no matter if an observer listens or not
-                 * That's why the value received by Observer#1 and Observer#2 are same but we don't see any output from Observer#3 because Observer#3 was listening after "hot.connect()"
-                 * Hot Observable doesn't care about its observer. When it doesn't have observer then the data has been emitted will be lost or an observer is late to listens then that observer will never get the data
-                 * Like a radio station, if we're late listening to an event then we'll miss it
-                 * */
-                val hot : ConnectableObservable<Long> = Observable.create<Long> {
-                    it.onNext(Random.nextLong(100))
-                    it.onComplete()
-                }.publish()
+                 * Maybe is similar to Single but it allows your observable the ability to not emit any item at all and it has additional method called "onComplete()"
+                 * "onSuccess()" will be called when Maybe emits an item
+                 * "onComplete()" will be called when Maybe doesn't emit any item at all
+                 **/
+                val maybe = Maybe.just(2)
+                // val maybe = Maybe.empty<Int>()
+                val maybeObserver = object: MaybeObserver<Int> {
+                    override fun onSubscribe(d: Disposable?) {
+                        Log.d("Reactive Programming", "Maybe onSubscribe!")
+                    }
 
-                hot.subscribe {
-                    Log.d("Reactive Programming", "Hot Observer#1 - onNext!: ${it}")
+                    override fun onSuccess(t: Int?) {
+                        Log.d("Reactive Programming", "Maybe onSuccess! ${t.toString()}")
+                    }
+
+                    override fun onError(e: Throwable?) {
+                        Log.d("Reactive Programming", "Maybe onError! ${e?.message}")
+                    }
+
+                    override fun onComplete() {
+                        Log.d("Reactive Programming", "Maybe onComplete!")
+                    }
                 }
 
-                hot.subscribe {
-                    Log.d("Reactive Programming", "Hot Observer#2 - onNext!: ${it}")
+                maybe.subscribe(maybeObserver)
+                /** --------------------------------- End | Maybe --------------------------------- **/
+
+                /** --------------------------------- Start | Completable --------------------------------- **/
+                /**
+                 * Completable is an Observable that only concerned about two things, if some action is executed ("onComplete()") or an error is encountered ("onError()")
+                 * Completable is useful for cases wherein you are only interested if something has executed properly, ignoring output or whatever.
+                 * Completable can be converted to an observable using toObservable. However, the reverse is also not straightforward. You can convert to a Single first
+                 * For example: There are certain scenario where only concern in completion or error. Suppose we update any User model in the app and want to just notify the server about it. We don’t care about the response because app already has the latest object
+                 **/
+                val completable = Completable.fromSingle(Single.just(1))
+                val completableObserver = object: CompletableObserver {
+                    override fun onSubscribe(d: Disposable?) {
+                        Log.d("Reactive Programming", "Completable onSubscribe!")
+                    }
+
+                    override fun onComplete() {
+                        Log.d("Reactive Programming", "Completable onComplete!")
+                    }
+
+                    override fun onError(e: Throwable?) {
+                        Log.d("Reactive Programming", "Completable onError! ${e?.message}")
+                    }
+
                 }
 
-                hot.connect()
-
-                hot.subscribe {
-                    Log.d("Reactive Programming", "Hot Observer#3 - onNext!: ${it}")
-                }
-
-                /** --------------------------------- End | Hot Observable --------------------------------- **/
+                completable.subscribe(completableObserver)
+                /** --------------------------------- End | Completable --------------------------------- **/
             }
         }
 
